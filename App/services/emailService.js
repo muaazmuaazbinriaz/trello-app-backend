eb3c41a62fd56376d061a115ddbd7faf88fd0cf0
muaaz <muaazbinriaz2000@gmail.com>
2026-01-29 20:33:41 +0500

    updated

--- App/services/emailService.js
+++ App/services/emailService.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
-
 dotenv.config();
-
 const transporter = nodemailer.createTransport({
   service: process.env.SMTP_SERVICE,
   auth: {
@@ -10,7 +8,6 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASS,
   },
 });
-// update
 const sendBoardInvite = async (to, boardTitle, boardId) => {
   const inviteLink = `${process.env.FRONTEND_URL}/home/${boardId}`;
   await transporter.sendMail({
@@ -19,7 +16,6 @@ const sendBoardInvite = async (to, boardTitle, boardId) => {
     subject: `Invitation to board :  "${boardTitle}"`,
     text: ` you are invited to join the board "${boardTitle}".\n\n${inviteLink}`,
     html: `
-
       <a href="${inviteLink}">Accept Invite</a>
     `,
   });
