5844efb4b03e0627869280096c4fa3f617a2a237
muaaz <muaazbinriaz2000@gmail.com>
2026-02-06 19:35:05 +0500

    updated

--- App/config/db.js
+++ App/config/db.js
@@ -1,28 +1,12 @@
 const mongoose = require("mongoose");
-let isConnected = false;
 
 const connectDB = async () => {
-  if (isConnected && mongoose.connection.readyState === 1) {
-    return;
-  }
-  if (mongoose.connection.readyState === 2) {
-    await new Promise((resolve) => {
-      mongoose.connection.once("connected", resolve);
-    });
-    isConnected = true;
-    return;
-  }
   try {
-    await mongoose.connect(process.env.DBURL, {
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 10000,
-    });
-    isConnected = true;
-    console.log("MongoDB connected...");
+    await mongoose.connect(process.env.DBURL);
+    console.log("MongoDB connected");
   } catch (err) {
-    isConnected = false;
-    console.log("MongoDB connection error:", err);
-    throw err;
+    console.error(err.message);
+    process.exit(1);
   }
 };
 
